7956e0469356585ebfd5e4fb680acf35480e7299
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2025-09-30 11:43:23 -0300

    implement Add method
    use match to separate when string is empty and other cases.
    Use string and list methods, to transform string into a list, then based on list size, sum the numbers

--- F#/Program.fs
+++ F#/Program.fs
@@ -1,2 +1,20 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// let text = "1,2"
+// let list = text.Split(",") |> Seq.toList
+// let result = List.map int list |> List.sum
+
+// printfn "calculation is: %d" result
+
+let Add (numbers: string) : int = 
+    match numbers with
+    | "" -> 0
+    | _ -> 
+    let numbers_list = numbers.Split "," |> Seq.toList
+    match numbers_list.Length with
+    | 1 -> int numbers_list[0]
+    | 2 -> int numbers_list[0] + int numbers_list[1]
+    // result -1 for a not considered case
+    | _ -> -1
+let test_cases = ["1,2";"1";"";"1,sd"]
+for test in test_cases do
+    printfn "result of \"%s\": %d" test (Add test)
+

da790ad21badf5f651415629c376813b53d4b9de
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2025-10-01 10:40:36 -0300

    Allow custom delimeters
    single character delimeters are supported.
    
    string is decomposed as a character list and then pattern matching is used to extract delimeter character

--- F#/Program.fs
+++ F#/Program.fs
@@ -4,12 +4,26 @@
 
 // printfn "calculation is: %d" result
 
-let Add (numbers: string) : int = 
-    match numbers with
+// if numbers_with_delimeter has delimiter, separates the delimeter from the input, and returns them separately in a tuple
+// else return the tuple (None, numbers_with_delimeter)
+let extract_delimeter (numbers_with_delimeter : string) : option<string> * string =
+    let char_list = numbers_with_delimeter.ToCharArray() |> Seq.toList
+    match char_list with
+        | '/'::'/'::delimeter::'\n' :: tail -> 
+            Some(delimeter.ToString()), // delimeter captured
+            Array.ofList tail |> System.String.Concat // rest of the numbers string
+        | _ -> None, numbers_with_delimeter
+
+let Add (numbers_with_delimeter: string) : int = 
+    match numbers_with_delimeter with
     | "" -> 0
     | _ -> 
+    let custom_delimeter, numbers = extract_delimeter numbers_with_delimeter
+    let delimeters = 
+        match custom_delimeter with
+        | Some d -> [|","; "\n"; d|]
+        | None -> [|","; "\n"|]
     let options = System.StringSplitOptions.TrimEntries
-    let delimeters = [|","; "\n"|]
     let numbers_list = numbers.Split(delimeters, options) |> Seq.toList
     try numbers_list |> List.map int |> List.sum with
     | ex ->
@@ -24,6 +38,8 @@ let test_cases = [
     "1,2,3,4"
     "1\n2,3"
     "1\n2,3\n4"
+    "//;\n1;2"
+    "//@\n1@2"
 ]
 for test in test_cases do
     printfn "result of \"%s\": %d" (test.Replace("\n", "\\n")) (Add test)

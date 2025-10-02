64d2f77cebb2d4a1b828205256071b82ba7072ae
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2025-10-02 13:42:25 -0300

    Ignore giant numbers
    create a new function to pass to the List.map method to allow for more complex behavior than just converting it to int

--- F#/Program.fs
+++ F#/Program.fs
@@ -18,6 +18,15 @@ let extract_delimeter (numbers_with_delimeter : string) : option<string> * strin
 let extract_negatives (numbers_list: list<int>): list<int> =
     List.filter (fun n -> n<0) numbers_list
 
+// extract the value of a number as a string
+// returns 0 when number is too big
+let extract_value (snumber: string): int =
+    let number = int snumber
+    let (|Valid|Giant|) n = if n > 1000 then Giant else Valid
+    match number with
+        | Giant -> 0
+        | Valid -> number
+
 let Add (numbers_with_delimeter: string) : int = 
     match numbers_with_delimeter with
     | "" -> 0
@@ -29,12 +38,12 @@ let Add (numbers_with_delimeter: string) : int =
         | None -> [|","; "\n"|]
     let options = System.StringSplitOptions.TrimEntries
     let numbers_list = numbers.Split(delimeters, options) |> Seq.toList
-    let int_list = numbers_list |> List.map int
+    let int_list = numbers_list |> List.map extract_value
     let negative_numbers = extract_negatives int_list
     if List.length negative_numbers > 0 then
         failwith ("negatives not allowed: " + System.String.Join(", ", negative_numbers))
     else
-    int_list |> List.sum
+    int_list |> List.sum // return the sum of the numbers
 
 let test_cases = [
     "1,2"
@@ -49,6 +58,8 @@ let test_cases = [
     "1,-2,-3"
     "1,-2, 2, 3, -5"
     "-0"
+    "2,1001"
+    "1,1000,50,2"
 ]
 for test in test_cases do
     try 

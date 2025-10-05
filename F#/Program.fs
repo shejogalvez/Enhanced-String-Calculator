71e6b9a2dcc837b72253e4d0558b68a077fbdead
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2025-10-05 14:17:02 -0300

    full regex solution

--- F#/Program.fs
+++ F#/Program.fs
@@ -4,37 +4,68 @@
 
 // printfn "calculation is: %d" result
 
-// if numbers_with_delimeter has delimiter, separates the delimeter from the input, and returns them separately in a tuple
-// else return the tuple (None, numbers_with_delimeter)
-let extract_delimeter (numbers_with_delimeter : string) : option<string> * string =
-    let char_list = numbers_with_delimeter.ToCharArray() |> Seq.toList
-    match char_list with
-        | '/'::'/'::delimeter::'\n' :: tail -> 
-            Some(delimeter.ToString()), // delimeter captured
-            Array.ofList tail |> System.String.Concat // rest of the numbers string
-        | _ -> None, numbers_with_delimeter
+open System.Text.RegularExpressions
 
+let separate_numbers_with_delimeters (numbers_with_delimeter : string) : option<string> * string = 
+    let delimeter_match = Regex.Match(numbers_with_delimeter, "//(.*)\n")
+    if not delimeter_match.Success
+    then None, numbers_with_delimeter
+    else 
+    let delimeter_str = delimeter_match.Groups[1].Value 
+    let numbers = numbers_with_delimeter.Replace(delimeter_match.Value, "")
+    Some delimeter_str, numbers
+
+
+// get a delimeter expression and returns an array of delimeter strings
+let parse_delimeters (delimeter_str : option<string>) : array<string> =
+    match delimeter_str with
+    | None -> [||]
+    | Some delimeter_expr_string ->
+    // get the string between // and \n
+    (match delimeter_expr_string.Length with
+    | 1 -> [|delimeter_expr_string|] // single character delimeter captured
+    | _ -> // complex delimeters
+    let multiple_matches = Regex.Matches(delimeter_expr_string, "\[(.*?)\]")
+    if multiple_matches.Count > 0 
+    then 
+        let delimeters_array = multiple_matches |> Seq.toArray |> Array.map (fun m -> m.Groups[1].Value)
+        delimeters_array
+    else
+    failwithf "non valid delimeter expression: %s" delimeter_expr_string)
 
 let extract_negatives (numbers_list: list<int>): list<int> =
     List.filter (fun n -> n<0) numbers_list
+
+// extract the value of a number as a string
+// returns 0 when number is too big
+let extract_value (snumber: string): int =
+    let number = int snumber
+    let (|Valid|Giant|) n = if n > 1000 then Giant else Valid
+    match number with
+        | Giant -> 0
+        | Valid -> number
 
 let Add (numbers_with_delimeter: string) : int = 
     match numbers_with_delimeter with
     | "" -> 0
     | _ -> 
-    let custom_delimeter, numbers = extract_delimeter numbers_with_delimeter
-    let delimeters = 
-        match custom_delimeter with
-        | Some d -> [|","; "\n"; d|]
-        | None -> [|","; "\n"|]
+    // get delimeters
+    let delimeters_str, numbers = separate_numbers_with_delimeters numbers_with_delimeter
+    let custom_delimeters = parse_delimeters delimeters_str
+    let defalut_delimeters = [|","; "\n"|]
+    let delimeters = Array.append custom_delimeters defalut_delimeters
+
+    // split numbers and get values
     let options = System.StringSplitOptions.TrimEntries
     let numbers_list = numbers.Split(delimeters, options) |> Seq.toList
-    let int_list = numbers_list |> List.map int
+    let int_list = numbers_list |> List.map extract_value
+
+    // check for negative values
     let negative_numbers = extract_negatives int_list
     if List.length negative_numbers > 0 then
         failwith ("negatives not allowed: " + System.String.Join(", ", negative_numbers))
     else
-    int_list |> List.sum
+    int_list |> List.sum // return the sum of the numbers
 
 let test_cases = [
     "1,2"
@@ -49,6 +80,17 @@ let test_cases = [
     "1,-2,-3"
     "1,-2, 2, 3, -5"
     "-0"
+    "2,1001"
+    "1,1000,50,2"
+    "//[***]\n1***2***3"
+    "//[*][%]\n1*2%3"
+    "//[*][%]\n1*2%3\n4,5"
+    "//[%][*]\n1*2%3\n4,5"
+    "//[***][%%]\n1***2%%3"
+    "//[***][%%]\n1***2%%3\n4,5"
+    "//[***][%%][***]\n1***2%%3"
+    "//[***][%%]\n1***2%%3,,5"
+    "//***,%%\n1***2%%3,,5"
 ]
 for test in test_cases do
     try 
